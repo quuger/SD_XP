3c4b1664202cebdff588124b3f9f2315db759cdf
Vinogradov Ilya <52038168+domosedy@users.noreply.github.com>
2026-03-30 16:30:17 +0300

    feat: add grpc client (#6)
    Co-authored-by: domosedy <ilayvin05@mail.ru>

---
+++ src/grpc_client.py
@@ -0,0 +1,43 @@
+import threading
+import time
+from collections.abc import Callable
+
+import grpc
+
+from proto import chat_pb2, chat_pb2_grpc
+
+
+class GrpcClient:
+    def __init__(
+        self,
+        host: str,
+        port: int,
+        username: str,
+        callback: Callable[[chat_pb2.ChatMessage], None],
+    ):
+        channel = grpc.insecure_channel(f"{host}:{port}")
+        self.connection = chat_pb2_grpc.ChatServerStub(channel)
+        self.callback = callback
+        self.username = username
+        threading.Thread(target=self.listen_threads, daemon=True).start()
+
+    def _listen_threads(self):
+        stream = self.connection.ChatStream(chat_pb2.Empty())
+        for note in stream:
+            self.callback(note)
+
+    def send_message(self, message: str):
+        if message != "":
+            proto_message = chat_pb2.ChatMessage(
+                name=self.username, text=message, timestamp=time.time_ns()
+            )
+
+            self.connection.SendMessage(proto_message)
+
+
+if __name__ == "__main__":
+    uid = int(input("Enter your user ID: "))
+    client = GrpcClient("localhost", 50051, str(uid), print)
+    for i in range(10):
+        client.send_message(f"Message number {i}")
+        time.sleep(2)
